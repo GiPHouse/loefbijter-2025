f957ab7531fe041541c4434c8f12ef433cfa6cec
Nofontnl <nofontnl@gmail.com>
2025-02-24 12:30:19 +0100

    Started work on index page

---
+++ loefsys/indexpage/__init__.py
@@ -0,0 +1,4 @@
+"""Django app for indexpage.
+
+In this app, the index page is defined.
+"""
---
+++ loefsys/indexpage/apps.py
@@ -0,0 +1,9 @@
+"""Module containing the configuration for the indexpage app."""
+
+from django.apps import AppConfig
+
+
+class IndexpageConfig(AppConfig):
+    """The app configuration for the indexpage."""
+
+    name = "loefsys.indexpage"
