7b96e72fc5d72ce145c2b7a2a289a30e1417a64b
Printer876 <rudolf.hilgeman2004@gmail.com>
2025-03-10 10:23:35 +0100

    added files that weren't merged into main from reservations branch

--- loefsys/urls.py
+++ loefsys/urls.py
@@ -2,7 +2,7 @@
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path("", views.main, name="main"),
     path("signup/", views.signup, name="signup_page"),
     path("admin/", admin.site.urls),
+    path("reservations/", include("loefsys.reservations.urls"))
     *debug_toolbar_urls(),
 ]
