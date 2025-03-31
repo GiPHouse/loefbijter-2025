50dbfc578f05b770a36f92625ba8fcff3e35f130
nofontnl <awesomeslime12@gmail.com>
2025-03-31 21:53:10 +0200

    Added tests for account information editing

--- loefsys/accountinfopage/tests.py
+++ loefsys/accountinfopage/tests.py
@@ -3,10 +3,97 @@
 from datetime import date
 
 from django.test import Client, TestCase
+from django.urls import reverse
 from django_dynamic_fixture import G
 
 from loefsys.groups.models import LoefbijterGroup
 from loefsys.users.models import MemberDetails, Membership, User
+
+
+class EditAccountInformationTestCase(TestCase):
+    """Tests for account information modification for users."""
+
+    def setUp(self):
+        """Set up users and constants."""
+        self.client = Client()
+        self.user = G(
+            User,
+            email="user@user.nl",
+            password="secret1",
+            phone_number="+31612345678",
+            nickname="A",
+        )
+        self.member = G(
+            MemberDetails, user=self.user, birthday=date(2004, 1, 1), show_birthday=True
+        )
+        self.membership = G(Membership, member=self.member, start=date(2024, 1, 1))
+        self.edit_url = reverse("accountinfoedit")
+
+    def test_valid_form_submitted(self):
+        """Test that when a valid form is submitted.
+
+        The data should be changed.
+        """
+        form_data = {
+            "phone_number": "+31612345000",
+            "nickname": self.user.nickname,
+            "display_name_preference": self.user.display_name_preference,
+            "birthday": date(2003, 1, 1),
+            "gender": self.member.gender,
+            "show_birthday": self.member.show_birthday,
+        }
+        self.client.force_login(user=self.user)
+        response = self.client.post(reverse("accountinfoedit"), data=form_data)
+        self.assertEqual(response.status_code, 302)  # expect redirect to account page
+        response = self.client.get("/account/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response=response, text="+31612345000")
+        self.assertContains(response=response, text="2003-01-01")
+
+    def test_invalid_form_submitted(self):
+        """Test that when an invalid form is submitted.
+
+        The data should not be changed (user form is incorrect, member form is okay).
+        """
+        form_data = {
+            "phone_number": "+316123450001",
+            "nickname": self.user.nickname,
+            "display_name_preference": self.user.display_name_preference,
+            "birthday": date(2003, 1, 1),
+            "gender": self.member.gender,
+            "show_birthday": self.member.show_birthday,
+        }
+        self.client.force_login(user=self.user)
+        response = self.client.post(reverse("accountinfoedit"), data=form_data)
+        self.assertEqual(response.status_code, 200)  # expect to load same page again
+        response = self.client.get("/account/")
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response=response, text="+316123450001")
+        self.assertContains(response=response, text="+31612345678")
+        self.assertNotContains(response=response, text="2003-01-01")
+        self.assertContains(response=response, text="2004-01-01")
+
+    def test_invalid_form_submitted_2(self):
+        """Test that when an invalid form is submitted.
+
+        The data should not be changed (user form is okay, member form is incorrect).
+        """
+        form_data = {
+            "phone_number": "+31612345000",
+            "nickname": self.user.nickname,
+            "display_name_preference": self.user.display_name_preference,
+            "birthday": date(2003, 1, 1),
+            "show_birthday": self.member.show_birthday,
+        }
+        self.client.force_login(user=self.user)
+        response = self.client.post(reverse("accountinfoedit"), data=form_data)
+        self.assertEqual(response.status_code, 200)  # expect to load same page again
+        response = self.client.get("/account/")
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response=response, text="+31612345000")
+        self.assertContains(response=response, text="+31612345678")
+        self.assertNotContains(response=response, text="2003-01-01")
+        self.assertContains(response=response, text="2004-01-01")
 
 
 class UserTestCase(TestCase):
@@ -77,7 +164,9 @@ class MemberTestCase(TestCase):
         self.user = G(
             User, email="member@member.nl", password="secret", phone_number="0687654321"
         )
-        self.member = G(MemberDetails, user=self.user, birthday=date(2004, 1, 1))
+        self.member = G(
+            MemberDetails, user=self.user, birthday=date(2004, 1, 1), show_birthday=True
+        )
 
     def test_member(self):
         """Test that all member information is displayed."""
