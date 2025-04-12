f61aa34972fc45798a62915491260153588a2a63
oets22 <oets.klotz@ru.nl>
2025-04-12 13:39:08 +0200

    reservation pages

--- loefsys/reservations/views.py
+++ loefsys/reservations/views.py
@@ -1,6 +1,7 @@
 """Module defining the class-based views for the reservations."""
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
@@ -13,6 +14,7 @@ class ReservationListView(LoginRequiredMixin, ListView):
     """Reservation list view."""
 
     model = Reservation
+    context_object_name = "reservations"
 
     def get_queryset(self):
         """Only show instances of Reservation made by the user."""
@@ -46,6 +48,8 @@ class ReservationDeleteView(LoginRequiredMixin, DeleteView):
     """Reservation delete view."""
 
     model = Reservation
+    template_name = "reservations/reservation_confirm_delete.html"
+    success_url = reverse_lazy("reservations")
 
 
 class ReservationDetailView(LoginRequiredMixin, DetailView):
