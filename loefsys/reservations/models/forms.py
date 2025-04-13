9586585ca9e3f59600180d117e21a56bbe464d2b
Michael Schrijer <m.schrijer@student.science.ru.nl>
2025-04-13 21:23:12 +0200

    Fixed merge, bug in HTML form and created a template for the menu.

--- loefsys/reservations/models/forms.py
+++ loefsys/reservations/models/forms.py
@@ -1,1 +1,27 @@
-"""Module defining the model for the reservation forms."""
+"""Module defining the forms for the reservations."""
+
+from django import forms
+
+from .models import ReservableItem, Reservation
+
+
+class CreateReservationForm(forms.ModelForm):
+    """A form to create reservations."""
+
+    reserved_item = forms.ModelChoiceField(queryset=ReservableItem.objects.all())
+    start = forms.DateTimeField(
+        input_formats=["%I:%M %p %d-%b-%Y"],
+        widget=forms.DateTimeInput(
+            attrs={"type": "datetime-local"}, format="%I:%M %p %d-%b-%Y"
+        ),
+    )
+    end = forms.DateTimeField(
+        input_formats=["%I:%M %p %d-%b-%Y"],
+        widget=forms.DateTimeInput(
+            attrs={"type": "datetime-local"}, format="%I:%M %p %d-%b-%Y"
+        ),
+    )
+
+    class Meta:
+        model = Reservation
+        fields = ("reserved_item", "start", "end")
